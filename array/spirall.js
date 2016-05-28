ec2b23a5e96873fcea260d6f90b812070a29123e
Shkarabura Igor <ishkarabura@admixer.ua>
2016-05-28 22:18:21 +0300

    spirall complete, 3 small task too

--- array/spirall.js
+++ array/spirall.js
@@ -7,35 +7,36 @@
  * build matrix and wrap in spirall, on clock arrow
  * */
 var start = document.getElementById('start'),
-    result = document.getElementById('result'),
-    arrBigStart = new Array(),
-    arrSmallStart = new Array(),
-    arrBigResult = new Array(),
-    arrSmallResult = new Array(),
-    count = 0,
-    maxValue = 10;
+    maxValue = 10,
+    tableResult = document.createElement('TABLE');
 
 for (var i = 0; i < maxValue; i++) {
-    arrSmallStart = [];
-    arrSmallResult = [];
+    var trResult = document.createElement('TR');
+    tableResult.appendChild(trResult);
     for (var j = 0; j < maxValue; j++) {
-        count++;
-        if (count.toString().length == 1) {
-            arrSmallStart.push(count + '_');
-
-        } else {
-            arrSmallStart.push(count);
-        }
-        arrSmallResult.push('+')
+        var tdResult = document.createElement('TD');
+        trResult.appendChild(tdResult);
     }
-    arrBigResult.push(arrSmallResult);
-    result.appendChild(document.createTextNode(arrSmallResult));
-    arrBigStart.push(arrSmallStart);
-    start.appendChild(document.createTextNode(arrSmallStart));
-    var br = document.createElement('br'),
-        br2 = document.createElement('br');
-    start.appendChild(br);
-    result.appendChild(br2);
+    var tr = document.getElementsByTagName('tr');
+}
+start.appendChild(tableResult);
 
 
+var increment= 0,decrement=maxValue- 1,ccount=0;
+
+for (var q = 0; q <= maxValue / 2; q++) {
+    for (var w = increment; w < decrement; w++) {
+        tr[increment].children[w].innerHTML = ccount++;
+    }
+    for (var e = increment; e < decrement; e++) {
+        tr[e].children[decrement].innerHTML = ccount++;
+    }
+    for (var r = decrement; r > increment; r--) {
+        tr[decrement].children[r].innerHTML = ccount++;
+    }
+    for (var t = decrement; t > increment; t--) {
+        tr[t].children[increment].innerHTML = ccount++;
+    }
+    increment++;
+    decrement--;
 }
