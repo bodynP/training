debfadbd7e6712e2aebbd9db99475e3b1b9f0305
pahlavaubivca <pahlavaubivca@gmail.com>
2016-07-31 14:43:43 +0300

    change structure

---
+++ canvas/methods/shape.js
@@ -0,0 +1,15 @@
+/**
+ * Created by pahlava on 31.07.2016.
+ */
+
+(function(){
+    var shapeParameter = {};
+
+    function Shape(){}
+    Shape.prototype.draw = function(){
+
+    };
+    Shape.prototype.setParameters = function(param){
+
+    };
+})();
